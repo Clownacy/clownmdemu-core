896dd6bc28f162cf8c606438e14b2dcb339b8eee
Clownacy <Clownacy@users.noreply.github.com>
2025-05-01 18:22:09 +0100

    Remove an unused macro.

--- controller.h
+++ controller.h
@@ -30,7 +30,6 @@ typedef struct Controller
 } Controller;
 
 void Controller_Initialise(Controller *controller);
-#define Controller_SetButton(CONTROLLER, BUTTON, VALUE) (CONTROLLER)->buttons[(BUTTON)] = (VALUE)
 cc_u8f Controller_Read(Controller *controller, cc_u16f microseconds, Controller_Callback callback, const void *user_data);
 void Controller_Write(Controller *controller, cc_u8f value, cc_u16f microseconds);
 
