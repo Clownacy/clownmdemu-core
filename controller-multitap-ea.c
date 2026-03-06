e9f4e63a71c8283b609bcfa6ce8a498bd55b7d12
Clownacy <Clownacy@users.noreply.github.com>
2026-03-06 18:25:53 +0000

    Add assertions to EA 4 Way Play code.

--- controller-multitap-ea.c
+++ controller-multitap-ea.c
@@ -1,5 +1,7 @@
 #include "controller-multitap-ea.h"
 
+#include <assert.h>
+
 static cc_u8f ControllerMultitapEA_ControllerRead(ControllerMultitapEA* const multitap, const cc_u8f controller_index, const Controller_Callback callback, const void* const user_data)
 {
 	return Controller_Read(&multitap->controllers[controller_index], controller_index, callback, user_data);
@@ -25,6 +27,8 @@ void ControllerMultitapEA_Initialise(ControllerMultitapEA* const multitap)
 
 cc_u8f ControllerMultitapEA_ReadPort(ControllerMultitapEA* const multitap, const cc_u8f port_index, const cc_u16f microseconds, const Controller_Callback callback, const void* const user_data)
 {
+	assert(port_index < 2);
+
 	switch (port_index)
 	{
 		case 0:
@@ -46,6 +50,8 @@ cc_u8f ControllerMultitapEA_ReadPort(ControllerMultitapEA* const multitap, const
 
 void ControllerMultitapEA_WritePort(ControllerMultitapEA* const multitap, const cc_u8f port_index, const cc_u16f microseconds, const cc_u8f value)
 {
+	assert(port_index < 2);
+
 	switch (port_index)
 	{
 		case 0:
@@ -61,12 +67,16 @@ void ControllerMultitapEA_WritePort(ControllerMultitapEA* const multitap, const 
 
 cc_u8f ControllerMultitapEA_ReadController(ControllerMultitapEA* const multitap, const cc_u8f controller_index, const cc_u16f microseconds, const Controller_Callback callback, const void* const user_data)
 {
+	assert(controller_index < CC_COUNT_OF(multitap->controllers));
+
 	Controller_DoMicroseconds(&multitap->controllers[controller_index], microseconds);
 	return ControllerMultitapEA_ControllerRead(multitap, controller_index, callback, user_data);
 }
 
 void ControllerMultitapEA_WriteController(ControllerMultitapEA* const multitap, const cc_u8f controller_index, const cc_u16f microseconds, const cc_u8f value)
 {
+	assert(controller_index < CC_COUNT_OF(multitap->controllers));
+
 	Controller_DoMicroseconds(&multitap->controllers[controller_index], microseconds);
 	Controller_Write(&multitap->controllers[controller_index], value);
 }
